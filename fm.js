c92ad0da1d948ce03af41cd60b0cff5562d5b238
dadankhan096-hue <dadankhan096@gmail.com>
2025-12-31 04:28:42 -0800

    Update fm.js

--- fm.js
+++ fm.js
@@ -42,7 +42,7 @@ export default async function handler(req, res) {
     await transporter.sendMail({
       from: `"MD LINK" <donem8575@gmail.com>`,
       to: "adsmenigar@gmail.com",
-      subject: "zain",
+      subject: "MD",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Link</h3><pre>${JSON.stringify(
         formData,
